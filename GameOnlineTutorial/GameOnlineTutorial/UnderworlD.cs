092b8deb3309edbf6eceb31022a372867e0db4eb
fruityloops5 <damian1234@abv.bg>
2015-12-16 15:19:09 +0200

    Update2
    Made a Sprite manager class.
    Holds all game logic of the game: collision, cannot go outside of box, list of enemies, mouse and keyboard input.
    Now every model that needs to be loaded into the game can be added directly from that class, no need to update the UnderworlD class.

--- GameOnlineTutorial/GameOnlineTutorial/UnderworlD.cs
+++ GameOnlineTutorial/GameOnlineTutorial/UnderworlD.cs
@@ -13,21 +13,11 @@ namespace UnderworlD
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        SpriteManager spriteManager;
         
-        Texture2D mainChar;
-        Texture2D enemy1;
         
-        Collision collision = new Collision();
         
-        //main char logic
-        Vector2 mainCharPosition = Vector2.Zero;
-        readonly int mainCharCollisionRectOffSet = 10;
-        readonly Point mainCharFrameSize = new Point(100, 100); // pixel X pixel , 1st one divided by roughly 3, second one is height default
-        private float mainCharSpeed = 5;
-        //enemy1 logic
-        Vector2 enemy1Position = new Vector2(300, 300);
-        int enemy1CollisionRectOffSet = 10;
-        Point enemy1FrameSize = new Point(100, 100);
+        
         
         //variables for key logic
 
@@ -49,7 +39,8 @@ namespace UnderworlD
         {
             // TODO: Add your initialization logic here
 
-
+            spriteManager = new SpriteManager(this);
+            Components.Add(spriteManager);
             base.Initialize();
         }
 
@@ -61,8 +52,6 @@ namespace UnderworlD
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            mainChar = Content.Load<Texture2D>("MainChar");
-            enemy1 = Content.Load<Texture2D>("enemy1");
 
             // TODO: use this.Content to load your game content here
         }
@@ -86,14 +75,7 @@ namespace UnderworlD
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            KeyboardState keyboardState = Keyboard.GetState();
-            CharMovement(keyboardState);
-            DetectOutOfBox();
             
-            if (collision.Collide(mainCharPosition, mainCharCollisionRectOffSet, mainCharFrameSize, enemy1Position, enemy1CollisionRectOffSet, enemy1FrameSize))
-            {
-                Exit();
-            }
             base.Update(gameTime);
             
             // TODO: Add your update logic here
@@ -110,59 +92,10 @@ namespace UnderworlD
             // TODO: Add your drawing code here
             
 
-            spriteBatch.Begin();
-            spriteBatch.Draw(mainChar, mainCharPosition, Color.White);
-            spriteBatch.Draw(enemy1, enemy1Position, Color.White);
-            spriteBatch.End();
-
             base.Draw(gameTime);
         }
 
-        //main char movemenet
-        private void CharMovement(KeyboardState keyboardState)
-        {
-            if (keyboardState.IsKeyDown(Keys.Left) || keyboardState.IsKeyDown(Keys.A))
-            {
-                mainCharPosition.X -= mainCharSpeed;
-            }
 
-            if (keyboardState.IsKeyDown(Keys.Right) || keyboardState.IsKeyDown(Keys.D))
-            {
-                mainCharPosition.X += mainCharSpeed;
-            }
-
-            if (keyboardState.IsKeyDown(Keys.Up) || keyboardState.IsKeyDown(Keys.W))
-            {
-                mainCharPosition.Y -= mainCharSpeed;
-            }
-
-            if (keyboardState.IsKeyDown(Keys.Down) || keyboardState.IsKeyDown(Keys.S))
-            {
-                mainCharPosition.Y += mainCharSpeed;
-            }
-        }
-        //detection for sprite to stay in the screen
-        private void DetectOutOfBox()
-        {
-            if (mainCharPosition.X < 0)
-            {
-                mainCharPosition.X = 0;
-            }
-            if (mainCharPosition.Y < 0)
-            {
-                mainCharPosition.Y = 0;
-            }
-
-            if (mainCharPosition.X > Window.ClientBounds.Width - mainCharFrameSize.X)
-            {
-                mainCharPosition.X = Window.ClientBounds.Width - mainCharFrameSize.X;
-            }
-
-            if (mainCharPosition.Y > Window.ClientBounds.Height - mainCharFrameSize.Y)
-            {
-                mainCharPosition.Y = Window.ClientBounds.Height - mainCharFrameSize.Y;
-            }
-        }
     }
 }
 
